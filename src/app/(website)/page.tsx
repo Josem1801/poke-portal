3f2639c80285c298d1dd9cd5d6c09703b75e0b26
Josem1801 <Josemanuelrosales2@gmail.com>
2025-06-30 03:32:08 -0600

    style: responsive home page

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -17,18 +17,18 @@ export default function App() {
     <div className="bg-gray-500 text-white w-full">
       <section className="relative w-full">
         <div className="absolute inset-0 bg-gradient-to-br from-red-900/30 via-red-800/20 to-transparent" />
-        <div className="w-full relative max-w-6xl mx-auto py-60">
+        <div className="w-full relative max-w-6xl min-h-[850px] mx-auto py-40 sm:py-60">
           <PokemonCarrousel />
         </div>
 
       </section>
       <section className="pt-40 gap-10 flex flex-col max-w-6xl mx-auto px-10 bg-gray-500 w-full">
-        <header className="flex items-center justify-between">
-          <Heading variant="h2">
+        <header className="flex items-center ">
+          <Heading variant="h2" className="flex-1">
             Characters Essentials
           </Heading>
           <Link passHref href="/pokemons">
-            <Button as="a" variant="primary" className=" gap-2 flex">
+            <Button as="a" variant="primary" className="w-fit gap-2 flex">
               See all
               {' '}
               <ArrowRight />
@@ -38,15 +38,18 @@ export default function App() {
         <CharactersEssentials />
       </section>
       <section className="pt-40 pb-80 gap-10 flex flex-col max-w-6xl mx-auto px-10 bg-gray-500 w-full">
-        <header className="flex items-center justify-between">
-          <Heading variant="h3">
+        <header className="flex items-center ">
+          <Heading variant="h3" className="flex-1">
             Game Essentials
           </Heading>
-          <Button variant="primary" className=" gap-2 flex">
-            See all
-            {' '}
-            <ArrowRight />
-          </Button>
+          <Link passHref href="/games">
+            <Button variant="primary" className=" gap-2 flex">
+              See all
+              {' '}
+              <ArrowRight />
+            </Button>
+          </Link>
+
         </header>
         <GamesEssentials />
       </section>
